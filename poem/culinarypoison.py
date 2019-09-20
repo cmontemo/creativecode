0b4712d459a3ddc4b093c7cdceb3f9958bc6f53b
cmontemo <54561828+cmontemo@users.noreply.github.com>
2019-09-20 08:39:49 -0400

    Update culinarypoison.py

--- poem/culinarypoison.py
+++ poem/culinarypoison.py
@@ -26,3 +26,75 @@ for i in range(1,11):
   s += countdown + 1
   print(" ".join(wordlist[s:s+countdown]))
   countdown -= 1
+
+  
+# Using this chunk of code instead of the above
+# generates a poem that has 10 lines of randomly-selected chunks of words.
+
+#seeed = random.randint(0,len(wordlist)-1)
+#wordlist[seed:seed+5]
+#line10=wordlist[seeed:seeed+10]
+#print(" ".join(line10) )
+
+# seed = random.randint(0,len(wordlist)-1)
+#wordlist[seed:seed+5]
+#line1=wordlist[seed:seed+9]
+#print(" ".join(line1) )
+
+#seed1 = random.randint(0,len(wordlist)-1)
+#wordlist[seed:seed+5]
+#line2=wordlist[seed1:seed1+8]
+#print(" ".join(line2) )
+
+#seed2 = random.randint(0,len(wordlist)-1)
+#wordlist[seed:seed+5]
+#line3=wordlist[seed2:seed2+7]
+#print(" ".join(line3) )
+
+#seed3 = random.randint(0,len(wordlist)-1)
+#wordlist[seed:seed+5]
+#line4=wordlist[seed3:seed3+6]
+#print(" ".join(line4) )
+
+#seed4 = random.randint(0,len(wordlist)-1)
+#wordlist[seed:seed+5]
+#line5=wordlist[seed4:seed4+5]
+#print(" ".join(line5) )
+
+#seed5 = random.randint(0,len(wordlist)-1)
+#wordlist[seed:seed+5]
+#line6=wordlist[seed5:seed5+4]
+#print(" ".join(line6) )
+
+#seed6 = random.randint(0,len(wordlist)-1)
+#wordlist[seed:seed+5]
+#line7=wordlist[seed6:seed6+3]
+#print(" ".join(line7) )
+
+#seed7 = random.randint(0,len(wordlist)-1)
+#wordlist[seed:seed+5]
+#line8=wordlist[seed7:seed7+2]
+#print(" ".join(line8) )
+
+#seed8 = random.randint(0,len(wordlist)-1)
+#wordlist[seed:seed+5]
+#line9=wordlist[seed8:seed8+1]
+#print(" ".join(line9) )
+
+
+# Old code
+# If you run this, it selects a 10-word chunk of text
+# and prints the same line, removing another word each time.
+
+#from textblob import TextBlob
+#blob = TextBlob(text)
+#poison = blob.words[random.randint(0,400)]
+
+#n = random.randint(0,30000)
+#randomWord = wordlist[n]
+
+#c =  10
+#for i in range(10):
+  
+# print (" ".join(wordlist[n:n+c]))
+#   c -= 1
