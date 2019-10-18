3cd22922a6814c5a7c0883d1a499563a193d0d41
cmontemo <54561828+cmontemo@users.noreply.github.com>
2019-10-18 12:37:48 -0400

    Update forthewall.js

--- screensaver/forthewall.js
+++ screensaver/forthewall.js
@@ -1,165 +1,139 @@
-let bricks = [];
-let logos = [];
-let umw;
+// setting up variables
+let counter = 0;
+let blocks = new Array();
 
-function preload(){
- umw = loadImage('umwlogo.png');
-  
+let eye;
+let hed;
+let head;
+
+let x = 40;
+let y = 0;
+let x2 = window.innerWidth
+let y2 = 0
+
+
+// loads images
+function preload() {
+  eye = loadImage('eye.jpg');
+  hed = loadImage('hed.png');
+  head = loadImage('head.png');
 }
 
+
+// sets up background, canvas size that scales 
 function setup() {
-  noCursor();
-  noStroke();
-  createCanvas(1920, 1080);
-  wallMap = [
-    '111111100111111',
-    '111111100111111',
-    '111111000111111',
-    '111110000011111',
-    '111110000001111',
-    '111110000001111',
-    '111110000001111',
-    '111110000001111',
-    '111111000001111',
-    '111111110011111'
-  ];
-  
-
-
-  for (let r = 0; r < 10; r += 1) {
-    for (let c = 0; c < 15; c += 1) {
-      if (wallMap[r][c] == '1') {
-        bricks.push(new Brick(c * 128, r * 108));
-      }
-    }
-  }
-
-  for (let l = 0; l < 1; l += 1) {
-    logos.push(new Logo());
-  }
-
+  background(0);
+  createCanvas(window.innerWidth, window.innerHeight);
+  image(eye,0,0);
+  image(hed,10,10,200,267);
+  image(head,155,70,300,400);
 }
+
 
 function draw() {
-  colorMode(RGB, 255);
-  background(0, 0, 0, 100);
   
-  for (let b of bricks) {
-    b.draw();
+//drawing images on canvas
+  //image(head, x2,y2,head.width/5, head.height/5);
+  image(hed,10,10,200,267);
+  image(head,155,70,300,400);
+  
+//adding text, used guide: https://p5js.org/reference/#/p5/text
+textSize(50);
+text('wake me up', 350, 200);
+text('wake',80,300);
+text('me',80,360);
+text('up',80,420);
+text('inside',80, 500);
+  
+textSize(150);
+text('SAVE ME',350,400);
+fill(200, 102, 153);
+// textAlign(CENTER,CENTER);
+  
+//star array design by Zach Whalen: https://editor.p5js.org/zachwhalen/sketches/Ll6g4jb7- 
+  if (counter % 5 == 0){
+    blocks.push( new Block(random(0,width),random(0,height)) );
   }
 
-  for (let l of logos) {
-    l.draw();
+  for (let s = 0; s < blocks.length; s++){
+    blocks[s].draw();
   }
+  
+  counter += 1;
 }
 
-function Brick(x, y) {
-  this.x = x - 64;
-  this.y = y;
+function Block(x,y){
+   this.x = x;
+   this.y = y;
 
-  this.draw = function() {
-
-    rectMode(CORNER);
-    fill(100, 0, 140,50);
-    rect(this.x, this.y, 128, 108);
-
-  }
+   
+//drawing blocks as 25x10 pixels
+   this.draw = function (){
+       fill(232,230,210);
+       noStroke();
+       rect(this.x,this.y,25,10);
+   }
 }
-
-function Logo() {
-  this.x = width / 2;
-  this.y = height / 2;
-  this.w = 75;
-  this.h = 75;
-  this.speedX = random(-6, 6);
-  this.speedY = random(-6, 6);
-  this.hue = random(20, 90);
-  this.draw = function() {
+ 
 
 
+// The following are snippets of code that I tried to use in various ways.  I heep it here so that I can revisit it later and perhaps implement some of my ideas.
 
-    // bounce off the walls
-    if (this.x < this.w / 2 || this.x > width - this.w / 2) {
-      this.speedX = -this.speedX;
-      this.hue = random(20, 90);
-    }
-    if (this.y > height - this.h / 2 || this.y < this.h / 2) {
-      this.speedY = -this.speedY;
-      this.hue = random(20, 90);
-    }
 
-    for (let b = 0; b < bricks.length; b++) {
+//making eyeball heads appear in array (instead of blocks), using Zach Whalen's star array example as a (attempted) starting point
+// let heds = new Array();
 
-      if (
-        this.x + this.speedX > bricks[b].x - this.w / 2 &&
-        this.x + this.speedX < bricks[b].x + 128 + this.w / 2 &&
-        this.y + this.speedY > bricks[b].y - this.h / 2 &&
-        this.y + this.speedY < bricks[b].y + 108 + this.h / 2
-      ) {
+// if (counter % 20 == 0){
+// heds.push( new Hed(img(random (0,width),random(0,height)))  )
+// }
 
-        // the next move will be contact, so figure out which
-        // edge it crosses to get there.
-        // the move vector is this.x,this.y -> this.x + this.speedX, this.y + this.speedY
-        if (
-          // top edge first:
-          intersects(this.x, this.y, this.x + this.speedX, this.y + this.speedY,bricks[b].x - this.w / 2 - 5, bricks[b].y - this.h / 2, bricks[b].x + 128 + this.w / 2 + 5, bricks[b].y - this.h / 2)
-        ) {
-          //print("TOP EDGE BREACHED");
-          this.speedY = -this.speedY;
-          this.hue = random(20, 90);
-        } else if (
-          // right edge:
-          intersects(this.x, this.y, this.x + this.speedX, this.y + this.speedY, bricks[b].x + 128 + this.w / 2, bricks[b].y - this.h / 2 - 5, bricks[b].x + 128 + this.w / 2, bricks[b].y + 108 + this.h / 2 + 5)
-        ) {
+// for (let s = 0; s < heds.length; s++){
+//   heds[s].draw();
+// }
 
-          this.speedX = -this.speedX;
-          this.hue = random(20, 90);
-        } else if (
-          // bottom edge
-          intersects(this.x, this.y, this.x + this.speedX, this.y + this.speedY,
-            bricks[b].x + 128 + this.w / 2 + 5, bricks[b].y + 108 + this.h / 2, bricks[b].x - this.w / 2 - 5, bricks[b].y + 108 + this.h / 2)
-        ) {
-          this.speedY = -this.speedY;
-          this.hue = random(20, 90);
-        } else if (
-          // left edge 
-          intersects(this.x, this.y, this.x + this.speedX, this.y + this.speedY, bricks[b].x - this.w / 2, bricks[b].y + 108 + 5, bricks[b].x - this.w / 2, bricks[b].y - this.h / 2 - 5)) {
+// function Hed(img,x,y)
+//   this.x = x;
+//   this.y = y;
 
-          this.speedX = -this.speedX;
-          this.hue = random(20, 90);
-        }
+// this.draw = function (){
+//   image(this.x, this.y, 150, 200);
+//      }
 
-      }
-    }
-    this.x += this.speedX;
-    this.y += this.speedY;
-    colorMode(HSB, 100);
-    fill(this.hue, 100, 70);
-    rectMode(CENTER);
-    rect(this.x, this.y, this.w, this.h);
-    imageMode(CENTER);
-    image(umw,this.x, this.y, this.w,this.h);
 
-  }
-}
+   
+// from random color squares by ivymeadows: https://editor.p5js.org/ivymeadows/sketches/S13gG2GhW; wanted to have randomly-placed blocks in random colors, need to figure out how to keep color static and not fluctuating
+//     randomColor = color(random(255),random(255),random(255));
+//     fill(randomColor);
+//     noStroke();
 
-// Something from Stackoverflow by Dan Fox
-// https://stackoverflow.com/questions/9043805/test-if-two-lines-intersect-javascript-function
-// returns true iff the line from (a,b)->(c,d) intersects with (p,q)->(r,s)
-function intersects(a, b, c, d, p, q, r, s) {
-  var det, gamma, lambda;
-  det = (c - a) * (s - q) - (r - p) * (d - b);
-  if (det === 0) {
-    return false;
-  } else {
-    lambda = ((s - q) * (r - a) + (p - r) * (s - b)) / det;
-    gamma = ((b - d) * (r - a) + (c - a) * (s - b)) / det;
-    return (0 < lambda && lambda < 1) && (0 < gamma && gamma < 1);
-  }
-}
+     //}
 
-function keyPressed(){
- if (keyCode == '32'){
-   logos.push(new Logo());
- }
-}
+
+// // mine - fills canvas full of aligned blocks
+// function draw() {
+//   background(220);
+  
+  // for (var x = 0; x <= width; x += 50){
+  //   for (var y = 0; y <= height; y += 20)
+  //   rect(x,y,50,20);
+  //   fill(232,230,210)
+//   }
+// }
+
+
+//part of my attempt at screenshot manipulation
+//let extraCanvas;
+
+// function setup() {
+//   createCanvas(600,375);
+//   img = loadImage('p5jsscreen-1.png');
+//   //extraCanvas = createGraphics(400,400);
+//   //extraCanvas.background(255);
+// }
+
+// function draw() {
+//   background(0);
+//   image(img, 0, 0);
+//   fill(103,45,85,255);
+//   ellipse(mouseX,mouseY,50,50);
+// }
